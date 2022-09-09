151a9ce2668b4551712961f4e053950376b8519d
Chris Troutner <chris.troutner@gmail.com>
2022-09-09 16:46:42 -0700

    Getting closer to token creation

--- src/components/create-token/index.js
+++ src/components/create-token/index.js
@@ -5,6 +5,7 @@
 // Global npm libraries
 import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Pin, Write } from 'p2wdb/index.js'
 
 class CreateToken extends React.Component {
   constructor (props) {
@@ -16,7 +17,8 @@ class CreateToken extends React.Component {
       tokenTicker: '',
       tokenUrl: '',
       tokenIcon: '',
-      tokenMetadata: ''
+      tokenMetadata: '',
+      appData: props.appData
     }
 
     // Bind the 'this' object to the event handlers.
@@ -164,6 +166,50 @@ class CreateToken extends React.Component {
 
     // Validate input
     this.validateInputs()
+
+    // Reinitilize the wallet UTXOs.
+    const bchWallet = this.state.appData.bchWallet
+    await bchWallet.initialize()
+
+    // Prepare to write data to the P2WDB
+    const wif = bchWallet.walletInfo.privateKey
+    const serverURL = 'http://localhost:5010'
+    const write = new Write({ bchWallet, serverURL })
+
+    // Upload the MSP JSON to IPFS.
+    const mspData = {
+      tokenIcon: "https://bafybeicvlcwv3flrwa4egmroyicvghevi6uzbd56drmoerjeguu4ikpnhe.ipfs.dweb.link/psf-logo.png",
+      about: "Demo token using this npm library: https://www.npmjs.com/package/slp-mutable-data"
+    }
+    const appId = 'token-data-001'
+    const result1 = await write.postEntry(mspData, appId)
+    const zcid1 = result1.hash
+    console.log('zcid1: ', zcid1)
+
+    // Ask the P2WDB to upload the JSON content to IPFS.
+    const pin = new Pin({ bchWallet, serverURL })
+    const cid1 = await pin.json(zcid1)
+    console.log('msp CID: ', cid1)
+
+    // Refresh the utxos in the wallet.
+    await bchWallet.bchjs.Util.sleep(2000)
+    await bchWallet.initialize()
+
+    // Pay the P2WDB to the CID.
+    const result2 = await pin.cid(cid1)
+    console.log('result2: ', result2)
+    const zcid2 = result2.hash
+    console.log('zcid2: ', zcid2)
+    // cid1 is the MSP IPFS CID that should be used.
+
+    /*
+      ToDo:
+       - p2wdb Write needs to return the same data structure weather it's paid
+       in BCH or in PSF tokens.
+       - This web app needs a way to generate a new key pair and save the index
+       to LocalStorage.
+       - The slp-mutable-data library can be used to generate a new token.
+    */
   }
 
   // Verify that the required inputs have been filled out.

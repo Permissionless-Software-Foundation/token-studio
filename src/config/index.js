c3393da7aa1315a4d5c06e51eb8a6383ca59dc0d
Chris Troutner <chris.troutner@gmail.com>
2022-09-20 15:47:59 -0700

    fix(footer): Updating filecoin repo link

--- src/config/index.js
+++ src/config/index.js
@@ -15,7 +15,7 @@ const config = {
   apkUrl: 'https://github.com/Permissionless-Software-Foundation/bch-wallet-web3-android/blob/master/android/apk/bch-web3-wallet.apk',
   ghRepo: 'https://github.com/Permissionless-Software-Foundation/token-studio',
   radicleUrl: 'https://app.radicle.network/seeds/maple.radicle.garden/rad:git:hnrkd5cjwwb5tzx37hq9uqm5ubon7ee468xcy/remotes/hyyycncbn9qzqmobnhjq9rry6t4mbjiadzjoyhaknzxjcz3cxkpfpc',
-  filecoinRepo: 'https://bafybeihofduppvln7o6cfhp7ltzfydhnari7r4fcthetvxo4dlwoaz3k4y.ipfs.w3s.link/'
+  filecoinRepo: 'https://bafybeiar4mjd6xrcw7r4xdv453agq2xabmlhnvpefvmodbgt322l44tyrq.ipfs.w3s.link/'
 }
 
 export default config
